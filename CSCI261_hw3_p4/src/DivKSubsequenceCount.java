f954088ba15ff9b8aa16142326457bfd21aa00ef
Derek Brown <djb3718@rit.edu>
2014-10-09 20:22:42 -0400

    Created java file for algorithm implementation

---
+++ CSCI261_hw3_p4/src/DivKSubsequenceCount.java
@@ -0,0 +1,14 @@
+/*
+ * DivKSubsequenceCount.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * purpose	Find the number of all subsequences where no two consecutive
+ * 			elements have the same remainder when divided by k
+ */
+public class DivKSubsequenceCount {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end DivKSubsequenceCount
